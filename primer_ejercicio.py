8a976f91bdb354273783941804e9ff5e3b9e3bea
OmarVelMart <32707107+OmarVelMart@users.noreply.github.com>
2024-04-30 19:35:35 +0000

    MiPrimerEjercicio

---
+++ primer_ejercicio.py
@@ -0,0 +1,24 @@
+#https://www.python.org/
+
+#Comentario en una linea
+
+"""Esto es Comentario
+en varias lineas
+"""
+
+'''Otra manera de generar
+comentarios en varias lineas
+'''
+
+my_variable = "Hola mundo"
+
+MY_CONSTANT = "Mi constante" # por convención
+
+my_int = 28
+my_float = 1.60
+my_bool = False
+my_bool = True
+my_string = "Hola desde acá"
+
+print(my_variable)
+
